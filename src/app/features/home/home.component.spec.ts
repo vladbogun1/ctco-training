cfa0f6103c848362fed50a533a6b47ef11eed650
vbogun <vladbogun@gmail.com>
2025-05-27 23:41:13 +0400

    feat(bogun-dev): test fix - Added test fix

--- src/app/features/home/home.component.spec.ts
+++ src/app/features/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import {ActivatedRoute} from "@angular/router";
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +9,17 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [HomeComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              data: {}
+            }
+          }
+        }
+      ]
     })
     .compileComponents();
 
